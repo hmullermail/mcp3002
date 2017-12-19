95457f881925af0264e42a37618816550c154bf3
Henrique Vasconcelos <hmullermail@gmail.com>
2017-12-18 22:35:15 -0300

    Now ok

--- app/adc_test3.py
+++ app/adc_test3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import spidev
 import string
 import time
 import os
@@ -12,35 +13,25 @@ channel_1        = 1               # ADC Channel 1
 delay            = 1               # Delay between readings
 measurements     = 5               # Number of readings for average value
 
-SPI = require('spi');
-
-#spi = new SPI.Spi('../../../dev/spidev0.0', 0, 0);
 
 # Open SPI bus
-# spi = spidev.SpiDev()
-# spi.open(0,0)
-# spi.max_speed_hz = 100000
-# spi.mode = 0
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 100000
+spi.mode = 0
 
 # Function to read SPI data from MCP3002 chip
 # Channel must be an integer 0|1
-
 def ReadChannel(channel):
   data           = 0
-  
-  for i in range(0,measurements):
-    #adc          = spi.xfer2([192,0])
-    #adc         = spi.xfer2([1,(2+channel)<<6,0])
-    adc = 255
-    
-    #data         += int(((adc[0]&3) << 8) + adc[1])
-    #data        += ((adc[1]&31) << 6) + (adc[2] >> 2)
-
-    time.sleep(0.2)
-    print("{} | {}".format(i, adc))
-
-  data           = float(data) / measurements
-  return data
+  cmd = 192
+  if channel:
+    cmd += 32
+  adc = spi.xfer2([cmd, 0])
+  reply_bitstring = ''.join(bitstring(n) for n in adc)
+  reply = reply_bitstring[5:15]
+  spi.close()
+  return int(reply, 2) / 2**10
 
 while True:
   # Read the light sensor data
@@ -53,23 +44,3 @@ while True:
 
   # Wait before repeating loop
   time.sleep(delay)
-
-
-
-#   var SPI = require('spi');
- 
-# var spi = new SPI.Spi('/dev/spidev0.0', {
-#     'mode': SPI.MODE['MODE_0'],  // always set mode as the first option 
-#     'chipSelect': SPI.CS['none'] // 'none', 'high' - defaults to low 
-#   }, function(s){s.open();});
- 
-# var txbuf = new Buffer([ 0x23, 0x48, 0xAF, 0x19, 0x19, 0x19 ]);
-# var rxbuf = new Buffer([ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ]);
- 
-# spi.transfer(txbuf, rxbuf, function(device, buf) {
-#     // rxbuf and buf should be the same here 
-#     var s = "";
-#     for (var i=0; i < buf.length; i++)
-#         s = s + buf[i] + " ";
-#         console.log(s + "- " + new Date().getTime());
-#   });

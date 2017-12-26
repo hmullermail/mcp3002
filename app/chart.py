8afe9f8ca75fc0b0839464436ff2681dc52e2425
Henrique Vasconcelos <hmullermail@gmail.com>
2017-12-26 02:05:28 -0300

    simple server example

--- app/chart.py
+++ app/chart.py
@@ -3,16 +3,20 @@ from flask import render_template
 from init import db, Reading, count_logs, reading_logs
 
 app = Flask(__name__)
-nopts = 8
+#nopts = 8
 
-@app.route("/")
-def chart():
-    labels = ["January","February","March","April","May","June","July","August"]
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+# @app.route("/")
+# def chart():
+#     labels = ["January","February","March","April","May","June","July","August"]
     
-    for i in range(0,nopts-1):
-    values[i] = i**2
-    #values[i] = reading_logs(count_logs-nopts)
-    return render_template('chart2.html', values=values, labels=labels)
+#     for i in range(0,nopts-1):
+#     values[i] = i**2
+#     #values[i] = reading_logs(count_logs-nopts)
+#     return render_template('chart2.html', values=values, labels=labels)
 
 if __name__ == '__main__':
     app.debug = True
